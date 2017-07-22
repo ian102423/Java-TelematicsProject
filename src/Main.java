7b27bc1d75497b13610c33e4ce819df60ec7b3e4
Ian Lee <ian102423@github.com>
2017-07-22 15:08:54 -0500

    Added Time

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) {
@@ -26,5 +28,32 @@ public class Main {
         vehInfo.setEngine(whatIsEngine);
 
         telSer.report(vehInfo);
+
+//        File file = new File(".");
+//        for (File f : file.listFiles()) {
+//            if (f.getName().endsWith(".json")) {
+//                // Now you have a File object named "f".
+//                // You can use this to create a new instance of Scanner
+//
+//
+//            }
+//        }
+//
+//        ObjectMapper mapper = new ObjectMapper();
+//        String json = mapper.writeValueAsString(vehicleInfo);
+//
+//        JSON to Java
+//
+//        ObjectMapper mapper = new ObjectMapper();
+//        VehicleInfo vi = mapper.readValue(json, VehicleInfo.class);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        LocalDateTime now = LocalDateTime.now();
+
+        String formatDateTime = now.format(formatter);
+
+        System.out.println("Current Time : " + formatDateTime);
+
     }
 }
